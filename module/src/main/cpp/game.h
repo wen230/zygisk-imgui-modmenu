cc95a55708684d969056165b146adf0e45670d0d
wen230 <88374188+wen230@users.noreply.github.com>
2023-07-17 01:36:29 +0800

    Update game.h

--- module/src/main/cpp/game.h
+++ module/src/main/cpp/game.h
@@ -3,6 +3,6 @@
 
 // TODO: change this
 #define TargetLibName "libil2cpp.so"
-#define GamePackageName "com.game.packagename"
+#define GamePackageName "com.oj8k.sg1001"
 
 #endif //ZYGISK_IMGUI_MODMENU_GAME_H
